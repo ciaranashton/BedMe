efd5f6a77f120f7fdf7cf55593c086e070a31eef
Stefan Petrov <sp929@bath.ac.uk>
2016-04-10 19:52:32 +0000

    Added tests for the property model

--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class PropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  def setup
+    @property = Property.new(addressLine1: "11 Alhalambra Street", addressLine2: "Floor 4",
+                     town: "Mombasa", postcode: "MB24AH")
+  end
+
+  test "should be valid" do
+    assert @property.valid?
+  end
+  
+   test "addres line 1 should be present" do
+    @property.addressLine1 = "     "
+    assert_not @property.valid?
+  end
+  
+   test "address line 1 should not be too long" do
+    @property.addressLine1 = "b" * 31
+    assert_not @property.valid?
+  end
+
+  test "address line 2 should not be too long" do
+    @property.addressLine2 = "b" * 31
+    assert_not @property.valid?
+  end
+   
+  test "town should be present" do
+    @property.town = ""
+    assert_not @property.valid?
+  end
+  
+  test "town shouldn't be too long" do
+    @property.town = "b"*51
+    assert_not @property.valid?
+  end
+  
+    test "postcode should be present" do
+    @property.postcode = ""
+    assert_not @property.valid?
+  end
+  
+  test "postcode shouldn't be too long" do
+    @property.postcode = "b"*9
+    assert_not @property.valid?
+  end
