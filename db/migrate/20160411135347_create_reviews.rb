cb9cd62fbf9746505a3c0b274c8a698e1ee32f0f
Stefan Petrov <sp929@bath.ac.uk>
2016-04-11 14:15:19 +0000

    Reviews schema change - made :user_property_period foreign key, primary key as well

--- db/migrate/20160411135347_create_reviews.rb
+++ db/migrate/20160411135347_create_reviews.rb
@@ -1,9 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews, id: false do |t|
-      t.references :user_property_period, index: true, null: false, foreign_key: true, unique: true
+      t.references :user_property_period, null: false, foreign_key: true
       t.text :reviewText
       t.timestamps null: false
     end
+    add_index :reviews, :user_property_period_id, unique: true
   end
 end
