7a2e92e57c73a62ba764a8d237cdca13e89c2a19
rjd40 <rjd40@bath.ac.uk>
2016-04-11 11:32:19 +0000

    Finished form validation

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -11,13 +11,21 @@ $(function() {
 	});
 	
 	$("input[type=email").on("change paste keyup input", function() {
-		validateField($(this), 
-			function(email) {
-				var rgxValidEmail = new RegExp("^[A-Z0-9\\-]+(?:\\.[A-Z0-9\\-]+)*@[A-Z0-9\\-]+(?:\\.[A-Z0-9\\-]+)*$", "i").exec(email);
-				return rgxValidEmail;
-			}
-			, "Invalid email address");
+		validateField($(this), function(email) {
+				return new RegExp("^[A-Z0-9\\-]+(?:\\.[A-Z0-9\\-]+)*@[A-Z0-9\\-]+(?:\\.[A-Z0-9\\-]+)+$", "i").exec(email);
+			}, "Invalid email address");
 	});
+	$("#txt_userPassword input, #txt_username input").on("change paste keyup input", function() {
+		validateField($(this), function(p) { return p.length > 0; }, "");
+	});
+	$("#txt_newPassword input").on("change paste keyup input", function() {
+		validateField($(this), function(p) { return p.length >=6; }, "Password is too short");
+		validateField($("#txt_confirmPassword input"), function(p) { return p == $("#txt_newPassword input").val(); }, "Passwords do not match");
+	});
+	$("#txt_confirmPassword input").on("change paste keyup input", function() {
+		validateField($(this), function(p) { return p == $("#txt_newPassword input").val(); }, "Passwords do not match");
+	});
+	
 });
 
 function validateField(input, validator, errorMessage) {
