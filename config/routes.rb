c37551e62cf90be6dc0e4dd43f4cdc134b015dca
Ciaran Ashton <ciaranashton@me.com>
2016-04-11 13:32:56 +0000

    Removed static pages - added to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root                 'static_pages#home'
-  get    'about'    => 'static_pages#about'
+  root                 'users#home'
+  get    'about'    => 'users#about'
   get    'signup'   => 'users#new'
   get    'login'    => 'sessions#new'
   post   'login'    => 'sessions#create'
   delete 'logout'   => 'sessions#destroy'
   resources :users
-  
+  resources :account_activations, only: [:edit]
 end
