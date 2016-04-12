dc472e26c800239319cd4360c6f50923a3512b3d
Ciaran Ashton <ciaranashton@me.com>
2016-04-12 13:23:06 +0000

    Added link to settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root                 'users#home'
   get    'about'    => 'users#about'
   get    'signup'   => 'users#new'
-  get    'settings' => 'users#edit'
   get    'login'    => 'sessions#new'
   post   'login'    => 'sessions#create'
   delete 'logout'   => 'sessions#destroy'
