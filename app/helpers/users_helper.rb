45d4a268c7f3fd822bf979a0888e63365c941dac
Ciaran Ashton <ciaranashton@me.com>
2016-04-12 13:51:18 +0000

    ,....

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,8 @@ module UsersHelper
   def gravatar_for(user)
     if logged_in?
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
   end
     
