cd13e16cdcc59d2f6c3fd8e7668b0e6987e40403
Stefan Petrov <sp929@bath.ac.uk>
2016-04-15 12:42:27 +0000

    Changed property add view to accomodate file uploads

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -32,15 +32,14 @@ class ImgUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+     process :resize_to_fit => [64, 64]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
