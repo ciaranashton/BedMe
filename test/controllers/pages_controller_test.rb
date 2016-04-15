b4a4bd934fedd0a8c1e220cb4e53ee97dc05ba5d
rjd40 <rjd40@bath.ac.uk>
2016-04-15 08:59:12 +0000

    Changed home and about page tests to reflect new controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
 end
