5c7fe43e0cd5e0283554d6c11ebfbadb5229a870
Stefan Petrov <sp929@bath.ac.uk>
2016-04-28 22:16:53 +0000

    Added validations to model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ActiveRecord::Base
     belongs_to :property
     belongs_to :user
+    validates :user, presence: true
+    validates :property, presence: true
+    validates :rating_area, presence: true
+    validates :rating_cleanliness, presence: true
+    validates :rating_cost, presence: true
+    validates :rating_landlord, presence: true
 end
