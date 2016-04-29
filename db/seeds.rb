8f846a7f174a646907d8edb02e9868f7d4feb52b
rjd40 <rjd40@bath.ac.uk>
2016-04-29 11:31:27 +0000

    Improved database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,22 @@ User.create!(name:  "Admin",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+             
+User.create!(name:  "Ciaran Ashton",
+             email: "ca448@bath.ac.uk",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name: "Robert Derbyshire",
+             email: "rjd40@bath.ac.uk",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
 
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@bed.me"
   password = "password"
@@ -18,22 +32,32 @@ User.create!(name:  "Admin",
                 activated_at: Time.zone.now)
 end
 
-User.create!(name:  "Ciaran Ashton",
-             email: "ciaran@bed.me",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             activated: true,
-             activated_at: Time.zone.now)
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+end
 
 Property.create!(addressLine1:  "47 New King Street",
                  addressLine2:  "Flat 4",
                  town:          "Bath",
                  county:        "Somerset",
-                 postcode:      "BA2 3BN")
+                 postcode:      "BA2 3BN",
+                 img:           seed_image("house1"))
+                 
+Property.create!(addressLine1:  "8 Lower Oldfield Park",
+                 addressLine2:  "Room 5",
+                 town:          "Bath",
+                 county:        "Somerset",
+                 postcode:      "BA2 3HL",
+                 img:           seed_image("house2"))
 
-properties = Property.order(:created_at).take(1)
-#users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  properties.each { |property| property.comments.create!(user_id: 101, content: content) }
+property = Property.first
+users_count = User.count
+
+5.times do |n|
+    randUser1 = User.second
+    content = Faker::Lorem.sentence(5)
+    property.comments.create!(user_id: randUser1.id, content: content)
+    randUser2 = User.third
+    content = Faker::Lorem.sentence(5)
+    property.comments.create!(user_id: randUser2.id, content: content)
 end
