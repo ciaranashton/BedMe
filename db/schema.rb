accb6b0bb39f37426c893834086a22a53967317d
Stefan Petrov <sp929@bath.ac.uk>
2016-04-29 13:27:23 +0000

    Added title to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416122409) do
+ActiveRecord::Schema.define(version: 20160429132137) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20160416122409) do
     t.text     "reviewText"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "reviewTitle"
   end
 
   add_index "reviews", ["property_id"], name: "index_reviews_on_property_id"
